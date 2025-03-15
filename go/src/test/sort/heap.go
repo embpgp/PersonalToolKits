df1986b4971eedf663d01135bf39a276d992ef66
rutk1t0r <pgpcsust@gmail.com>
2025-03-15 08:15:15 +0800

    stream topk

--- go/src/test/sort/heap.go
+++ go/src/test/sort/heap.go
@@ -1,5 +1,7 @@
 package sort
 
+import "fmt"
+
 func heapSort(a []int) {
 	n := len(a)
 	//先构建一个最大堆，从最后一个元素开始
@@ -41,4 +43,57 @@ func findTopKbyHeap(a []int, k int) int {
 		heapify(a, i, 0) //从0开始调整
 	}
 	return a[k]
+}
+
+type streamMinHeap struct {
+	k    int
+	data []int
+}
+
+// 从i节点下沉
+func (h *streamMinHeap) Down(i int) {
+	small := i
+	left := 2*i + 1
+	right := 2*i + 2
+	if left < len(h.data) && h.data[left] < h.data[i] {
+		small = left
+	}
+	if right < len(h.data) && h.data[right] < h.data[i] {
+		small = right
+	}
+	if small != i {
+		h.data[small], h.data[i] = h.data[i], h.data[small]
+		h.Down(small)
+	}
+}
+
+// 从i节点上浮
+func (h *streamMinHeap) Up(i int) {
+	for {
+		p := (i - 1) / 2
+		if p >= 0 && h.data[i] < h.data[p] {
+			h.data[i], h.data[p] = h.data[p], h.data[i]
+			i = p
+		} else {
+			break
+		}
+
+	}
+
+}
+
+func (h *streamMinHeap) Add(num int) {
+	if len(h.data) < h.k {
+		h.data = append(h.data, num)
+		h.Up(len(h.data) - 1)
+	} else {
+		if h.data[0] < num {
+			h.data[0] = num
+			h.Down(0)
+		}
+	}
+}
+
+func (h *streamMinHeap) Print() {
+	fmt.Printf("%v\n", h.data)
 }
--- go/src/test/sort/heap_test.go
+++ go/src/test/sort/heap_test.go
@@ -13,3 +13,18 @@ func Test_heapSort(t *testing.T) {
 	fmt.Printf("%+v", target)
 
 }
+
+func Test_streamMinHeap_Add(t *testing.T) {
+	k := 4
+	h := &streamMinHeap{
+		k:    k,
+		data: make([]int, 0, k+1),
+	}
+
+	target := []int{3, 230, 33, 23, 11, 34, 1, 0, 55, 5, 53, 304, 1000, 341, 231, 344}
+	for _, num := range target {
+		h.Add(num)
+		fmt.Printf("添加 %2d -> 当前Top%d:%v\n", num, h.k, h.data)
+	}
+
+}
